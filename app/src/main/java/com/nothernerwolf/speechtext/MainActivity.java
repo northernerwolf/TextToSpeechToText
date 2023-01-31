8c48f0ac5c3d1d97a6f6a3b7d333659d609fe4f6
Guwanc Haldurdyyew <guwanchaldurdyyew01@gmail.com>
2023-01-31 19:02:52 +0500

    speech to text finished

--- app/src/main/java/com/nothernerwolf/speechtext/MainActivity.java
+++ app/src/main/java/com/nothernerwolf/speechtext/MainActivity.java
@@ -2,13 +2,39 @@ package com.nothernerwolf.speechtext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button text_to_speech, speech_to_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_to_speech = findViewById(R.id.text_to_speech);
+        speech_to_text = findViewById(R.id.speech_to_text);
+
+
+        text_to_speech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),TextToSpeechActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        speech_to_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),SpeechToTextActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nothernerwolf/speechtext/SpeechToTextActivity.java
@@ -0,0 +1,14 @@
+package com.nothernerwolf.speechtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpeechToTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_to_text);
+    }
+}
